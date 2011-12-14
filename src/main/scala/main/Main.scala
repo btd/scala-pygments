40fc70e63bff4fda1275af079ba16ab1cbb7b8c5
Denis Bardadym <denis.bardadym@accenture.com>
2011-12-14 12:38:05 +0400

    Common Lisp Cython
    Signed-off-by: Denis Bardadym <denis.bardadym@accenture.com>

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -3,42 +3,31 @@ package main
 
 object Main extends App {
 	val code = """
-<cfif thisTag.executionMode EQ "start">
- <cfdirectory action="list" name="getImages" directory="#ExpandPath(ATTRIBUTES.directory)#">
- 
- <cfparam name="SESSION.picture" default="0">
- <cfset SESSION.picture = SESSION.picture + 1>
- 
- <cfif SESSION.picture GT getImages.RecordCount>
-  <cfset SESSION.picture = 1>
- </cfif>
- 
- <cfoutput> 
- <img src="#ATTRIBUTES.directory#/#getImages.name[SESSION.picture]#"
-  <cfif isDefined("ATTRIBUTES.height")>
-   height="#ATTRIBUTES.height#"
-  </cfif>
-  <cfif isDefined("ATTRIBUTES.width")>
-   width="#ATTRIBUTES.width#"
-  </cfif>
-  <cfif isDefined("ATTRIBUTES.alt")>
-   alt="#ATTRIBUTES.alt#"
-  <cfelse>
-   alt="#getImages.name[SESSION.picture]#"
-  </cfif>>
- </cfoutput>
-<cfelseif thisTag.executionMode EQ "end">
- <cfif isDefined("ATTRIBUTES.CommentBody") AND ATTRIBUTES.CommentBody EQ "true">
-  <cfset thisTag.GeneratedContent = "<!--#thisTag.GeneratedContent#-->">
- </cfif>
-</cfif> 
+def primes(int kmax):
+    cdef int n, k, i
+    cdef int p[1000]
+    result = []
+    if kmax > 1000:
+        kmax = 1000
+    k = 0
+    n = 2
+    while k < kmax:
+        i = 0
+        while i < k and n % p[i] != 0:
+            i = i + 1
+        if i == k:
+            p[k] = n
+            k = k + 1
+            result.append(n)
+        n = n + 1
+    return result
 """
 	var out_file = new java.io.FileOutputStream("test.html")
 	var out_stream = new java.io.PrintStream(out_file)
 
 	//Pygments.lex(code, new lexer.ScalaLexer(new LexerOptions ) )
 	out_stream.println("""<link type="text/css" rel="stylesheet" href="test.css">""")
-	Pygments.highlight(code, new lexer.ColdfusionHtmlLexer(new LexerOptions ), new formatters.HtmlFormatter )
+	Pygments.highlight(code, new lexer.CythonLexer(new LexerOptions ), new formatters.HtmlFormatter )
 		.foreach(t => out_stream.println(t))
 		
 
